b74c22dabc7a4c333962013112d7c08a875db8e8
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-10-25 18:03:43 +0200

    navn fra norg v0.1

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/tilgang/DevEntraController.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/tilgang/DevEntraController.kt
@@ -7,17 +7,23 @@ import no.nav.sikkerhetstjenesten.entraproxy.graph.AnsattId
 import no.nav.sikkerhetstjenesten.entraproxy.graph.AnsattOidTjeneste
 import no.nav.sikkerhetstjenesten.entraproxy.graph.EntraTjeneste
 import no.nav.sikkerhetstjenesten.entraproxy.felles.utils.cluster.ClusterConstants.DEV
+import no.nav.sikkerhetstjenesten.entraproxy.graph.Enhetsnummer
+import no.nav.sikkerhetstjenesten.entraproxy.norg.NorgTjeneste
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.client.RestClient
 
 @UnprotectedRestController(value = ["/${DEV}"])
 @ConditionalOnNotProd
 @Tag(name = "DevTilgangController", description = "Denne kontrolleren skal kun brukes til testing")
-class DevEntraController (private val entra: EntraTjeneste, private val oid: AnsattOidTjeneste) {
+class DevEntraController (private val entra: EntraTjeneste, private val oid: AnsattOidTjeneste, private val norgTjeneste: NorgTjeneste) {
 
     @GetMapping("ansatt/enheter/{ansattId}")
     fun enheter(@PathVariable ansattId: AnsattId) = entra.enheter(ansattId, oid.oidFraEntra(ansattId))
 
     @GetMapping("ansatt/tema/{ansattId}")
     fun tema(@PathVariable ansattId: AnsattId) = entra.tema(ansattId, oid.oidFraEntra(ansattId))
+
+    @GetMapping("ansatt/tema/{enhetsnummer}")
+    fun enhetNavn(@PathVariable enhetsnummer: Enhetsnummer) = norgTjeneste.navnFor(enhetsnummer)
 }

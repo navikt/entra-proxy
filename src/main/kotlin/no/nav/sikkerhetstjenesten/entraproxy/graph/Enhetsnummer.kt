decbd3a0f8e6578a88accfe660f60bde7146fc09
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-10-25 19:30:53 +0200

    chore

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/Enhetsnummer.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/Enhetsnummer.kt
@@ -4,11 +4,10 @@ import com.fasterxml.jackson.annotation.JsonValue
 import no.nav.sikkerhetstjenesten.entraproxy.felles.utils.extensions.DomainExtensions.requireDigits
 
 
-data class Enhet(val enhetsnummer: Enhetnummer, val navn: String) {
+data class Enhet(val enhetnummer: Enhetnummer, val navn: String) {
     data class Enhetnummer(@JsonValue val verdi: String) {
         init {
             requireDigits(verdi,4)
-           // require(verdi.length == 4) { "Enhetsnummer må være 4 siffer" }
         }
     }
 }

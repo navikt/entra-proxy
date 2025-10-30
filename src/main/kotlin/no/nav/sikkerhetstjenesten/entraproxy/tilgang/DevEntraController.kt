61bea9642966a2d5cf02d15bff0377412ea03c8f
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-10-30 13:07:59 +0100

    Nye devkontroller endepunkt for test

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/tilgang/DevEntraController.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/tilgang/DevEntraController.kt
@@ -11,6 +11,8 @@ import no.nav.sikkerhetstjenesten.entraproxy.graph.Enhet.Enhetnummer
 import no.nav.sikkerhetstjenesten.entraproxy.graph.Tema
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import java.util.UUID
 
 @UnprotectedRestController(value = ["/${DEV}"])
 @ConditionalOnNotProd
@@ -22,9 +24,21 @@ class DevEntraController (private val entra: EntraTjeneste, private val oid: Ans
 
     @GetMapping("ansatt/tema/{ansattId}")
     fun tema(@PathVariable ansattId: AnsattId) = oid.oid(ansattId).let { entra.tema(ansattId, it) }
-
+/**
     @GetMapping("gruppe/enheter/{enhetsnummer}")
+    fun enhetGrupper(@PathVariable enhetsnummer: Enhetnummer) = entra.gruppeIdForEnhet(enhetsnummer).let { entra.(enhetsnummer,it) }
     fun gruppeIdForEnhet(@PathVariable enhetsnummer: Enhetnummer) = entra.gruppeIdForEnhet(enhetsnummer)
+
+    @GetMapping("gruppe/tema/{temakode}")
+    fun temaGrupper(@PathVariable temakode: Tema) = entra.gruppeIdForTema(temakode).let { entra.temaMedlemmer(temakode, it) }
+
+
+    @GetMapping("gruppe/tema/{oid}")
+    fun gruppeMedOID(@PathVariable oid: UUID) = entra.temaMedlemmer(Tema("TSO"),oid)
+ **/
+@GetMapping("gruppe/oid/{enhetsnummer}")
+fun grupperOID(@PathVariable enhetsnummer: Enhetnummer) = entra.gruppeIdForEnhet(enhetsnummer)
+
 
     @GetMapping("gruppe/tema/{tema}")
     fun gruppeIdForTema(@PathVariable tema: Tema) = entra.gruppeIdForTema(tema)

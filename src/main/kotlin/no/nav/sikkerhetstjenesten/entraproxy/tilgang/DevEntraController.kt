4a4ab0f224587580108b9c71ac83f499f95a9322
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-10-30 13:48:57 +0100

    paths

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/tilgang/DevEntraController.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/tilgang/DevEntraController.kt
@@ -11,20 +11,18 @@ import no.nav.sikkerhetstjenesten.entraproxy.graph.Enhet.Enhetnummer
 import no.nav.sikkerhetstjenesten.entraproxy.graph.Tema
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.reactive.function.client.WebClientResponseException
-import java.util.UUID
 
 @UnprotectedRestController(value = ["/${DEV}"])
 @ConditionalOnNotProd
 @Tag(name = "DevEntraController", description = "Denne kontrolleren skal kun brukes til testing")
-class DevEntraController (private val entra: EntraTjeneste, private val oid: AnsattOidTjeneste) {
+class DevEntraController (private val entra: EntraTjeneste, private val oidTjeneste: AnsattOidTjeneste) {
 
     @GetMapping("ansatt/enheter/{ansattId}")
-    fun enheter(@PathVariable ansattId: AnsattId) = oid.oid(ansattId).let { entra.enheter(ansattId, it) }
+    fun enheter(@PathVariable ansattId: AnsattId) = oidTjeneste.oid(ansattId).let { entra.enheter(ansattId, it) }
 
-    @GetMapping("ansatt/tema/{ansattId}")
-    fun tema(@PathVariable ansattId: AnsattId) = oid.oid(ansattId).let { entra.tema(ansattId, it) }
-/**
+    @GetMapping("ansatt/temaer/{ansattId}")
+    fun temaer(@PathVariable ansattId: AnsattId) = oidTjeneste.oid(ansattId).let { entra.tema(ansattId, it) }
+    /**
     @GetMapping("gruppe/enheter/{enhetsnummer}")
     fun enhetGrupper(@PathVariable enhetsnummer: Enhetnummer) = entra.gruppeIdForEnhet(enhetsnummer).let { entra.(enhetsnummer,it) }
     fun gruppeIdForEnhet(@PathVariable enhetsnummer: Enhetnummer) = entra.gruppeIdForEnhet(enhetsnummer)
@@ -35,14 +33,14 @@ class DevEntraController (private val entra: EntraTjeneste, private val oid: Ans
 
     @GetMapping("gruppe/tema/{oid}")
     fun gruppeMedOID(@PathVariable oid: UUID) = entra.temaMedlemmer(Tema("TSO"),oid)
- **/
-@GetMapping("gruppe/oid/{enhetsnummer}")
-fun grupperOID(@PathVariable enhetsnummer: Enhetnummer) = entra.gruppeIdForEnhet(enhetsnummer)
+     **/
+    @GetMapping("gruppe/enhet/{enhetsnummer}")
+    fun gruppeIdForEnhet(@PathVariable enhetsnummer: Enhetnummer) = entra.gruppeIdForEnhet(enhetsnummer)
 
 
     @GetMapping("gruppe/tema/{tema}")
     fun gruppeIdForTema(@PathVariable tema: Tema) = entra.gruppeIdForTema(tema)
 
-    @GetMapping("ansatt/oid/{ansattId}")
-    fun oid(@PathVariable ansattId: AnsattId) = oid.oid(ansattId)
+    @GetMapping("ansatt/{ansattId}")
+    fun oid(@PathVariable ansattId: AnsattId) = oidTjeneste.oid(ansattId)
 }

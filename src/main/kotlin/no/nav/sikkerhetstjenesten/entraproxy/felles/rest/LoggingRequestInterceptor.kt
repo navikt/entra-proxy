40f1d181c358478b6a943171cac0b9c38322ab94
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-02 18:56:26 +0100

    tell token type

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/rest/LoggingRequestInterceptor.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/rest/LoggingRequestInterceptor.kt
@@ -1,5 +1,8 @@
 package no.nav.sikkerhetstjenesten.entraproxy.felles.rest
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.http.HttpRequest
@@ -9,12 +12,13 @@ import org.springframework.http.client.ClientHttpResponse
 import org.springframework.stereotype.Component
 
 @Component
-class LoggingRequestInterceptor : ClientHttpRequestInterceptor {
+class LoggingRequestInterceptor(private val teller: TokenTypeTeller) : ClientHttpRequestInterceptor {
     private val log = getLogger(javaClass)
     override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
         if (request.uri.path.contains("monitoring")) {
             return execution.execute(request, body)
         }
+        teller.tell()
        //log.trace("Headers for {}: {}", request.uri, request.headers/*.filter { !it.key.contains(AUTHORIZATION) }*/)
         if (!body.isEmpty()) {
           // log.debug("Body for {} {} : {} ",request.method, request.uri,String(body))
@@ -25,4 +29,21 @@ class LoggingRequestInterceptor : ClientHttpRequestInterceptor {
         }
         return response
     }
+}
+
+@Component
+class TokenTypeTeller(registry: MeterRegistry, token: Token) :
+    AbstractTeller(registry, token, "token.type", "Token type")
+
+abstract class AbstractTeller(private val registry: MeterRegistry, private val token: Token, private val navn: String, private val beskrivelse: String) {
+
+    open fun tell(tags: Tags = Tags.empty(), n:Int=1) =
+        Counter.builder(navn)
+            .description(beskrivelse)
+            .tags(tags
+                .and("token", TokenType.from(token).name.lowercase())
+                .and("system", token.system)
+                .and("clustersystem", token.clusterAndSystem))
+            .register(registry)
+            .increment(n.toDouble())
 }

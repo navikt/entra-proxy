f19da19f0bb54620278e2aee0e0f52234261d43a
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-04 13:18:55 +0100

    chore

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheOppfrisker.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheOppfrisker.kt
@@ -1,8 +1,7 @@
 package no.nav.sikkerhetstjenesten.entraproxy.felles.cache
 
-import no.nav.sikkerhetstjenesten.entraproxy.graph.EntraConfig.Companion.GRAPH
 
 interface CacheOppfrisker {
-     val cacheName: String get() = GRAPH
-    fun oppfrisk(nøkkelElementer: CacheNøkkelElementer) = Unit
+    val cacheName: String
+    fun oppfrisk(nøkkelElementer: CacheNøkkelElementer)
 }

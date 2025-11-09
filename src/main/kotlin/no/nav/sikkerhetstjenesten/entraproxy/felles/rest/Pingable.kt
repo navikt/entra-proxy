522d00ddca2270e9bdc13de8cccb66ee85191fd9
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-09 18:18:17 +0100

    nullable

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/rest/Pingable.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/rest/Pingable.kt
@@ -2,7 +2,7 @@ package no.nav.sikkerhetstjenesten.entraproxy.felles.rest
 
 interface Pingable {
 
-    fun ping(): Any
+    fun ping(): Any?
     val pingEndpoint: String
     val name: String
     val isEnabled get(): Boolean = true

01240fffeb56a7bb68f9ab21d7b93ef2479d9cbd
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-10 14:50:45 +0100

    no toString

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/Ansatt.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/Ansatt.kt
@@ -1,6 +1,7 @@
 package no.nav.sikkerhetstjenesten.entraproxy.graph
 
 import com.fasterxml.jackson.annotation.JsonValue
+import no.nav.sikkerhetstjenesten.entraproxy.felles.utils.extensions.DomainExtensions.requireDigits
 
 
 data class Ansatt(@JsonValue val id: String, val displayName: String): Comparable<Ansatt> {
@@ -9,7 +10,7 @@ data class Ansatt(@JsonValue val id: String, val displayName: String): Comparabl
         with(id) {
             require(length == ANSATTID_LENGTH) { "Ugyldig lengde $length for $this, forventet ${ANSATTID_LENGTH}" }
             require(first().isLetter()) { "Ugyldig første tegn ${first()} i $this, må være stor bokstav" }
-            no.nav.sikkerhetstjenesten.entraproxy.felles.utils.extensions.DomainExtensions.requireDigits(
+            requireDigits(
                 substring(1),
                 6
             )
@@ -22,5 +23,5 @@ data class Ansatt(@JsonValue val id: String, val displayName: String): Comparabl
         const val ANSATTID_LENGTH = 7
     }
 
-    override fun toString() = id
+    //override fun toString() = id
 }

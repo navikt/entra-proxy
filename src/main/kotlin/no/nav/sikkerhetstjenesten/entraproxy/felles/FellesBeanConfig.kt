57f2b5bc3876671f0f2a0db9a8560f85f70e7f2b
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-20 19:38:07 +0100

    4.0

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/FellesBeanConfig.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/FellesBeanConfig.kt
@@ -59,7 +59,7 @@ class FellesBeanConfig(private val ansattIdAddingInterceptor: ConsumerAwareHandl
     fun restClientCustomizer(interceptor: OAuth2ClientRequestInterceptor, tokenInterceptor: TokenTypeTellendeRequestInterceptor) =
         RestClientCustomizer { c ->
             c.requestFactory(HttpComponentsClientHttpRequestFactory().apply {
-                setConnectTimeout(2000)
+                setConnectionRequestTimeout(2000)
                 setReadTimeout(2000)
             })
             c.requestInterceptors {

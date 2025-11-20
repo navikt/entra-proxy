2555d92c253b457c95b609e1bcaae34fe0170856
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-20 10:38:41 +0100

    boot4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 val javaVersion = JavaLanguageVersion.of(21)
-val springdocVersion = "2.8.13"
-val tokenSupportVersion = "5.0.37"
+val springdocVersion = "3.0.0-RC1"
+val tokenSupportVersion = "6.0.0-SNAPSHOT"
 val mockkVersion = "1.14.6"
-
-
 
 group = "no.nav.sikkerhetstjenesten.entraproxy"
 version = "1.0.1"
@@ -14,7 +12,7 @@ plugins {
     val kotlinVersion = "2.2.20"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "4.0.0-RC2"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.cyclonedx.bom") version "2.4.1"
     id("com.google.cloud.tools.jib") version "3.4.5"
@@ -35,24 +33,21 @@ repositories {
 
 configurations.all {
     resolutionStrategy {
-        failOnNonReproducibleResolution()
+        //failOnNonReproducibleResolution()
     }
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.20.1")
     implementation("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:2.20.1-alpha")
-    implementation("io.micrometer:micrometer-core")
     implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("io.micrometer:micrometer-tracing")
-    implementation("io.opentelemetry:opentelemetry-api")
-    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
+    implementation("net.logstash.logback:logstash-logback-encoder:9.0")
     implementation("no.nav.boot:boot-conditionals:5.1.11")
     implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
     implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
     implementation("org.apache.httpcomponents.client5:httpclient5")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-cache")
@@ -60,7 +55,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.retry:spring-retry")
     implementation("org.springframework:spring-aspects")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
     testImplementation("io.mockk:mockk:$mockkVersion")
@@ -69,6 +63,21 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/navikt/token-support")
+        credentials {
+            username = findProperty("gpr.user") as String?
+            password = findProperty("gpr.key") as String?
+        }
+    }
+    maven {
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+}
 dependencyManagement {
     imports {
         mavenBom("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:2.20.1")
--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/rest/PingableHealthIndicator.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/rest/PingableHealthIndicator.kt
@@ -2,8 +2,8 @@ package no.nav.sikkerhetstjenesten.entraproxy.felles.rest
 
 
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.boot.actuate.health.Health
-import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.boot.health.contributor.Health
+import org.springframework.boot.health.contributor.HealthIndicator
 
 
 class PingableHealthIndicator(private val pingable: Pingable) : HealthIndicator {

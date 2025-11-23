55cca98766396938e4dc5d3b14f0265a7dc61e9e
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-23 19:55:56 +0100

    subclass

--- src/test/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/AnsattSerializationTest.kt
+++ src/test/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/AnsattSerializationTest.kt
@@ -1,26 +1,11 @@
 import tools.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
-import no.nav.sikkerhetstjenesten.entraproxy.graph.Ansatt
 import no.nav.sikkerhetstjenesten.entraproxy.graph.AnsattUtvidetInfo
 import tools.jackson.module.kotlin.jacksonObjectMapper
 
 class AnsattSerializationTest {
     private val objectMapper = jacksonObjectMapper()
 
-    @Test
-    fun `Ansatt serialiseres og deserialiseres korrekt`() {
-        val json = """
-            {
-              "id": "7a9cca1f-8552-4042-ad1a-255efb740991",
-              "onPremisesSamAccountName": "B654321",
-              "displayName": "Kari Nordmann",
-              "givenName": "Kari",
-              "surname": "Nordmann"
-            }
-        """.trimIndent()
-        val deserialized = objectMapper.readValue<Ansatt>(json)
-        println(deserialized)
-    }
     @Test
     fun `AnsattUtvidetInfo serialiseres og deserialiseres korrekt`() {
         val json = """

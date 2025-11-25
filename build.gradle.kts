d67443d128b44854ff9d84b3a9443c94e1f43b89
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-25 09:35:35 +0100

    use bom

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ configurations.all {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:$otelVersion")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations")
     implementation("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:2.20.1-alpha")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")

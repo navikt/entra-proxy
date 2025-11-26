f278aaa2ec48e34911dd8fc912fd141fef14ed9f
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-26 16:00:54 +0100

    log cache expiry

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheExpiredEventListener.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheExpiredEventListener.kt
@@ -2,6 +2,7 @@ package no.nav.sikkerhetstjenesten.entraproxy.felles.cache
 
 import io.micrometer.core.instrument.Tags.of
 import no.nav.sikkerhetstjenesten.entraproxy.felles.utils.LeaderAware
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.context.SmartLifecycle
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
@@ -10,11 +11,13 @@ import org.springframework.stereotype.Component
 @Component
 class CacheExpiredEventListener(val teller: CacheOppfriskerTeller, erLeder: Boolean = false, private vararg val oppfriskere: CacheOppfrisker = emptyArray()) :
     LeaderAware(erLeder), SmartLifecycle {
+    private val log = getLogger(javaClass)
     private var running = false
     @EventListener
     fun cacheInnslagFjernet(hendelse: CacheElementUtløptLytter.CacheInnslagFjernetHendelse) {
         if (erLeder && isRunning()) {
             val elementer = CacheNøkkelElementer(hendelse.nøkkel)
+            log.info("cache innslag utløpt for cache '${elementer.cacheName}' med nøkkel '${hendelse.nøkkel}'")
             oppfriskere.firstOrNull { it.cacheName == elementer.cacheName }?.run {
                 oppfrisk(elementer)
                 teller.tell(of("cache", elementer.cacheName, "result", "expired", "method", elementer.metode ?: "ingen"))

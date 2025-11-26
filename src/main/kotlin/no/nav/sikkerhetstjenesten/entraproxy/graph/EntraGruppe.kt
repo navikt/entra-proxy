e969ea823879acd8eeb81dfd30c3bc87a0ba98f7
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-11-26 17:06:18 +0100

    bytte navn på visningnavn

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/EntraGruppe.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/EntraGruppe.kt
@@ -1,5 +1,5 @@
 package no.nav.sikkerhetstjenesten.entraproxy.graph
 
-data class EntraGruppe(val verdi: String): Comparable<EntraGruppe> {
-    override fun compareTo(other: EntraGruppe): Int =verdi.compareTo(other.verdi)
+data class EntraGruppe(val rolle: String): Comparable<EntraGruppe> {
+    override fun compareTo(other: EntraGruppe): Int =rolle.compareTo(other.rolle)
 }

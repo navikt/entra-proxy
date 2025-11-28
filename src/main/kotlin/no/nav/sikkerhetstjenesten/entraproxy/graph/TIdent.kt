f220b43be3aecb8fa85ecbfdac902ab72e3fc58e
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-28 17:35:36 +0100

    chore

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/TIdent.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/TIdent.kt
@@ -3,19 +3,17 @@ package no.nav.sikkerhetstjenesten.entraproxy.graph
 import com.fasterxml.jackson.annotation.JsonValue
 import no.nav.sikkerhetstjenesten.entraproxy.felles.utils.extensions.Requirements.requireDigits
 
-data class TIdent (@JsonValue val verdi: String) : Comparable<TIdent> {
+data class TIdent (@JsonValue val verdi: String)  {
     init {
         with(verdi) {
-            require(length == ANSATTID_LENGTH) { "Ugyldig lengde $length for $this, forventet $ANSATTID_LENGTH" }
+            require(length == TIDENT_LENGTH) { "Ugyldig lengde $length for $this, forventet $TIDENT_LENGTH" }
             require(first().isLetter()) { "Ugyldig første tegn ${first()} i $this, må være stor bokstav" }
             requireDigits(substring(3), 4)
         }
     }
 
-    override fun compareTo(other: TIdent): Int = verdi.compareTo(other.verdi)
-
     companion object {
-        const val ANSATTID_LENGTH = 7
+        private const val TIDENT_LENGTH = 7
     }
 
     override fun toString() = verdi

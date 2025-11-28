985195cd41ea031d1ed3e711eb031fc57d8d0a86
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-28 12:33:59 +0100

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     id("java")
     id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.cyclonedx.bom") version "2.4.1"
+    id("org.cyclonedx.bom") version "3.0.0"
     id("com.google.cloud.tools.jib") version "3.4.5"
     application
 }

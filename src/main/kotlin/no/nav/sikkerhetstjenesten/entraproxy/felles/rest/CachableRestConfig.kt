c624e11fb8dcf4b424e5f66d637fd89a13c0ab72
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-29 21:40:55 +0100

    norg varighet cfg

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/rest/CachableRestConfig.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/rest/CachableRestConfig.kt
@@ -1,16 +1,16 @@
 package no.nav.sikkerhetstjenesten.entraproxy.felles.rest
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.time.Duration
 
 interface CachableRestConfig {
-    val varighet: Duration get() = Duration.ofHours(12)
+    val varighet: Duration get() = Duration.ofHours(3)
     val navn: String
 }
 
 @Component
-class MedlemmerCachableRestConfig() : CachableRestConfig {
-    override val varighet = Duration.ofHours(3)
+class MedlemmerCachableRestConfig(@param:Value("\${medlemmer.varighet:17h}") override val varighet: Duration) : CachableRestConfig {
     override val navn = MEDLEMMER
 
     companion object {

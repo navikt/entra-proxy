e70b51bd17301c96240be016b4393cc9cd3cfd2f
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2026-01-16 12:41:58 +0100

    spwan på norg-tjeneste

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/norg/NorgTjeneste.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/norg/NorgTjeneste.kt
@@ -1,5 +1,6 @@
 package no.nav.sikkerhetstjenesten.entraproxy.norg
 
+import io.opentelemetry.instrumentation.annotations.WithSpan
 import no.nav.sikkerhetstjenesten.entraproxy.graph.Enhet.Enhetnummer
 import no.nav.sikkerhetstjenesten.entraproxy.norg.NorgConfig.Companion.NORG
 import org.springframework.cache.annotation.Cacheable
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class NorgTjeneste(private val adapter : NorgRestClientAdapter) {
+    @WithSpan
     @Cacheable(cacheNames = [NORG],  key = "#root.methodName + ':' + #enhetnummer.verdi")
     fun navnFor(enhetnummer: Enhetnummer) = adapter.navnFor(enhetnummer.verdi)
 }

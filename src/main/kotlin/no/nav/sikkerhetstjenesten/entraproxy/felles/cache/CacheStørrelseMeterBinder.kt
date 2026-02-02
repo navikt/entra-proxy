6e3cfbb2c57eb157e10c61aa64c2c4669fa3dea8
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-02 15:42:55 +0100

    rapporter cache størrelse bare som leder

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheStørrelseMeterBinder.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheStørrelseMeterBinder.kt
@@ -12,7 +12,7 @@ class CacheStørrelseMeterBinder(private val client: CacheClient) :  LeaderAware
     override fun bindTo(registry: MeterRegistry)   {
         somLeder(0L) {
             client.cfgs.forEach { cfg ->
-                registry.gauge("cache.size", Tags.of("navn", cfg.navn), client) { handler ->
+                registry.gauge("cache.size", Tags.of("navn", cfg.navn), client) { _ ->
                     client.cacheStørrelse(cfg.navn)
                 }
             }

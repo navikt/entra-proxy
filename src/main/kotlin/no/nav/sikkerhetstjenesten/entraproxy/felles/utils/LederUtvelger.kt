66d0e49a3667e49ccdf40eaa48692c58ca020fb6
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-03 12:33:19 +0100

    samme lederutvikler-logikk som maskinen

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/utils/LederUtvelger.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/utils/LederUtvelger.kt
@@ -8,7 +8,6 @@ import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.event.ContextClosedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClient.Builder
 import org.springframework.web.reactive.function.client.WebClientRequestException
 import org.springframework.web.reactive.function.client.bodyToFlux

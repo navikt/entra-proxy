d75a6cc723d4d940460602a0f4de8ef2002a06c8
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-03 13:41:23 +0100

    log

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheStørrelseMeterBinder.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheStørrelseMeterBinder.kt
@@ -4,16 +4,20 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tags
 import io.micrometer.core.instrument.binder.MeterBinder
 import no.nav.sikkerhetstjenesten.entraproxy.felles.utils.LeaderAware
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.stereotype.Component
 
 @Component
 class CacheStørrelseMeterBinder(private val client: CacheClient) :  LeaderAware(), MeterBinder {
+    private val log = getLogger(javaClass)
 
     override fun bindTo(registry: MeterRegistry)   {
         somLeder(0L) {
             client.cfgs.forEach { cfg ->
                 registry.gauge("cache.size", Tags.of("navn", cfg.navn), client) { _ ->
-                    client.cacheStørrelse(cfg.navn)
+                    client.cacheStørrelse(cfg.navn).also {
+                        log.info("Cache størrelse for cache '${cfg.navn}': $it innslag" )
+                    }
                 }
             }
         }

c74a59750c60bddfb6fbf04f96832072b8a913e7
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-04-29 14:02:27 +0200

    pingable på en ny måte

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/norg/NorgClientBeanConfig.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/norg/NorgClientBeanConfig.kt
@@ -1,8 +1,12 @@
 package no.nav.sikkerhetstjenesten.entraproxy.norg
 
 import no.nav.sikkerhetstjenesten.entraproxy.felles.FellesBeanConfig.Companion.createClient
+import no.nav.sikkerhetstjenesten.entraproxy.felles.rest.Pingable
+import no.nav.sikkerhetstjenesten.entraproxy.felles.rest.PingableHealthIndicator
+import no.nav.sikkerhetstjenesten.entraproxy.norg.NorgConfig.Companion.NORG
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient.Builder
 import org.springframework.web.client.RestClient.ResponseSpec.ErrorHandler
 
@@ -13,6 +17,15 @@ class NorgClientBeanConfig {
     @Bean
     fun norgProxyClient( cfg: NorgConfig,b: Builder, errorHandler: ErrorHandler) =
         createClient<NorgProxyClient>(cfg, b, errorHandler)
+    @Bean
+    fun norgHealthIndicator(pingable: NorgPingable) =
+        PingableHealthIndicator(pingable)
     
 }
 
+@Component
+class NorgPingable(private val client: NorgProxyClient, cfg: NorgConfig) : Pingable {
+    override fun ping() = client.ping()
+    override val pingEndpoint = cfg.pingEndpoint.toString()
+    override val name = NORG
+}

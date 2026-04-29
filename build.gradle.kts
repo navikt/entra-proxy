d87d8ecc90992b0dd7ae7089ff8e6443e4b95d76
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-04-29 12:44:29 +0200

    jetty

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,6 +61,7 @@ configurations.all {
     resolutionStrategy {
         failOnNonReproducibleResolution()
     }
+    exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
 }
 
 dependencies {
@@ -79,7 +80,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
     implementation("org.springframework.boot:spring-boot-starter-restclient")
     implementation("org.springframework.boot:spring-boot-starter-webclient")
     implementation("org.springframework.boot:spring-boot-starter-webflux")

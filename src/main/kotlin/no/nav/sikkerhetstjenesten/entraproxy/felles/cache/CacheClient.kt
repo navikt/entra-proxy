e0b0c60a4b517bda11c4071d964bda3f11655b76
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-11 14:44:30 +0200

    fjernet ubrukte cache-metoder

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheClient.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheClient.kt
@@ -32,57 +32,6 @@ class CacheClient(client: RedisClient, private val adapter : CacheStørrelseAdap
     override fun delete(cache: CachableConfig, id: String) =
         conn.sync().del(handler.tilNøkkel(cache, id))
 
-
-    @WithSpan
-    override fun <T : Any> getOne(cache: CachableConfig,id: String, clazz: KClass<T>): T? =
-        conn.sync().get(handler.tilNøkkel(cache, id))?.let { json ->
-            handler.fraJson(json, clazz)
-        }
-
-    @WithSpan
-    override fun putOne(cache: CachableConfig, id: String, value: Any, ttl: Duration) {
-        conn.async().setex(handler.tilNøkkel(cache, id), ttl.seconds, handler.tilJson(value))
-    }
-
-
-    @WithSpan
-    override fun <T : Any> getMany(cache: CachableConfig, ids: Set<String>, clazz: KClass<T>): Map<String, T> =
-        if (ids.isEmpty()) {
-            emptyMap()
-        } else {
-            conn.sync()
-                .mget(*ids.map { id -> handler.tilNøkkel(cache, id) }.toTypedArray<String>())
-                .filter { it.hasValue() }
-                .associate { handler.idFraNøkkel(it.key) to handler.fraJson(it.value, clazz) }
-
-        }
-
-    @WithSpan
-    override fun putMany(cache: CachableConfig, innslag: Map<String, Any>, ttl: Duration) {
-        if (innslag.isNotEmpty()) {
-            log.trace("Bulk lagrer {} verdier for cache {} med prefix {}", innslag.size, cache.name, cache.extraPrefix)
-            conn.apply {
-                with(payloadFor(innslag, cache)) {
-                    setAutoFlushCommands(false)
-                    async().mset(this)
-                    keys.forEach { key ->
-                        async().expire(key, ttl.seconds)
-                    }
-                }
-                flushCommands()
-                setAutoFlushCommands(true)
-            }
-        }
-    }
-
-    private fun payloadFor(innslag: Map<String, Any>, cache: CachableConfig) =
-        buildMap {
-            innslag.forEach { (key, value) ->
-                put(handler.tilNøkkel(cache, key), handler.tilJson(value))
-            }
-        }
-
-
     fun cacheStørrelser() =
         cfgs.associate {
             it.navn to "${cacheStørrelse(it.navn)} innslag, ttl: ${it.varighet.format()}"
--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheOperations.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheOperations.kt
@@ -1,13 +1,6 @@
 package no.nav.sikkerhetstjenesten.entraproxy.felles.cache
 
-import java.time.Duration
-import kotlin.reflect.KClass
-
 interface CacheOperations {
     fun delete(cache: CachableConfig, id: String): Long
-    fun <T : Any> getOne(cache: CachableConfig,id: String, clazz: KClass<T>): T?
-    fun putOne(cache: CachableConfig, id: String, value: Any, ttl: Duration)
-    fun <T : Any> getMany(cache: CachableConfig, ids: Set<String>, clazz: KClass<T>): Map<String, T?>
-    fun putMany(cache: CachableConfig, innslag: Map<String, Any>, ttl: Duration)
     fun tilNøkkel(cache: CachableConfig, id: String): String
 }

d1bfda9f49bc5405e389240568249d23c448fa6b
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-16 11:49:12 +0200

    toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,10 @@
+import org.gradle.api.tasks.testing.Test
+import org.gradle.testing.jacoco.tasks.JacocoReport
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 import java.lang.System.getProperty
 import kotlin.KotlinVersion.Companion.CURRENT
 
 val javaVersion = JavaLanguageVersion.of(26)
-val springdocVersion = "3.0.3"
-val tokenSupportVersion = "6.0.6"
-val kotestVersion = "6.1.11"
-val mockkVersion = "1.14.9"
-val logstashEncoderVersion = "9.0"
-val springMockVersion = "5.0.1"
-val conditionalsVersion = "6.0.5"
-val coroutinesVersion = "1.10.2"
-val otelVersion = "2.28.0"
 val git = grgit
 
 group = "no.nav.sikkerhetstjenesten.entraproxy"
@@ -19,20 +12,19 @@ version = "1.0.1"
 
 
 plugins {
-    val kotlinVersion = "2.4.0"
     id("jacoco")
-    id("org.ajoberstar.grgit") version "5.3.3"
-    id("org.jetbrains.dokka") version "2.2.0"
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "4.1.0"
-    id("io.spring.dependency-management") version "1.1.7"
-    id("org.cyclonedx.bom") version "3.2.4"
-    id("io.kotest") version "6.1.11"
+    alias(libs.plugins.grgit)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.kotlinSpring)
+    alias(libs.plugins.springBoot)
+    alias(libs.plugins.springDependencyManagement)
+    alias(libs.plugins.cyclonedx)
+    alias(libs.plugins.kotest)
     application
 }
 
-tasks.jacocoTestReport {
+tasks.named<JacocoReport>("jacocoTestReport") {
     dependsOn(tasks.test)
     classDirectories.setFrom(
         files(classDirectories.files.map {
@@ -71,7 +63,7 @@ repositories {
 }
 
 
-configurations.all {
+configurations.configureEach {
     resolutionStrategy {
         failOnNonReproducibleResolution()
     }
@@ -79,55 +71,60 @@ configurations.all {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations")
-    implementation("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:$otelVersion-alpha")
-    implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
-    implementation("no.nav.boot:boot-conditionals:$conditionalsVersion")
-    implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
-    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
-    implementation("org.apache.httpcomponents.client5:httpclient5")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-cache")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web") {
-        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
-    }
-    implementation("org.springframework.boot:spring-boot-starter-jetty")
-    implementation("org.springframework.boot:spring-boot-starter-restclient")
-    implementation("org.springframework.boot:spring-boot-starter-webclient")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework:spring-aspects")
-    testImplementation("com.ninja-squad:springmockk:$springMockVersion")
-    testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-restclient-test")
-    testImplementation("io.kotest:kotest-runner-junit5:${kotestVersion}")
-    testImplementation("io.kotest:kotest-assertions-core:${kotestVersion}")
-    testImplementation("io.kotest:kotest-extensions-spring:${kotestVersion}")
+    // Kotlin
+    implementation(libs.kotlinxCoroutinesCore)
+    implementation(libs.kotlinReflect)
+
+    // Observability and logging
+    implementation(libs.opentelemetryInstrumentationAnnotations)
+    implementation(libs.opentelemetryLogbackMdc)
+    implementation(libs.micrometerRegistryPrometheus)
+    implementation(libs.logstashLogbackEncoder)
+
+    // NAV and security
+    implementation(libs.bootConditionals)
+    implementation(libs.tokenClientSpring)
+    implementation(libs.tokenValidationSpring)
+
+    // HTTP and API documentation
+    implementation(libs.httpclient5)
+    implementation(libs.springdocOpenapiStarterWebmvcUi)
+
+    // Spring Boot starters
+    implementation(libs.springBootStarterActuator)
+    implementation(libs.springBootStarterCache)
+    implementation(libs.springBootStarterDataRedis)
+    implementation(libs.springBootStarterJetty)
+    implementation(libs.springBootStarterRestclient)
+    implementation(libs.springBootStarterValidation)
+    implementation(libs.springBootStarterWeb)
+    implementation(libs.springBootStarterWebclient)
+    implementation(libs.springBootStarterWebflux)
+    implementation(libs.springAspects)
+
+    // Testing
+    testImplementation(libs.springMockk)
+    testImplementation(libs.mockk)
+    testImplementation(libs.junitJupiter)
+    testImplementation(libs.bundles.springBootTest)
+    testImplementation(libs.bundles.kotest)
     testImplementation(kotlin("test"))
 }
 
 dependencyManagement {
     imports {
-        mavenBom("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:$otelVersion")
+        mavenBom("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:${libs.versions.otel.get()}")
     }
 }
 
 application {
     mainClass.set("no.nav.sikkerhetstjenesten.entraproxy.EntraProxyApplicationKt")
 }
-tasks.withType<BootJar> {
+tasks.named<BootJar>("bootJar") {
     archiveFileName = "app.jar"
 }
 
-tasks.test {
+tasks.named<Test>("test") {
     jvmArgs("--add-opens", "java.base/java.util=ALL-UNNAMED")
     useJUnitPlatform()
 }
@@ -139,7 +136,7 @@ java {
 }
 
 kotlin {
-    jvmToolchain(javaVersion.asInt())
+    jvmToolchain(26)
 
     compilerOptions {
         freeCompilerArgs.add("-Xjsr305=strict")

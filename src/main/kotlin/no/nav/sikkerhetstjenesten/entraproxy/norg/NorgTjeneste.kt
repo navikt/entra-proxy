15eaa2c6449a508a2575b85234680ad91474689d
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-15 18:28:23 +0200

    chore

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/norg/NorgTjeneste.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/norg/NorgTjeneste.kt
@@ -13,6 +13,4 @@ import org.springframework.web.service.registry.ImportHttpServices
 class NorgTjeneste(private val client: NorgProxyClient) {
     @Cacheable(cacheNames = [NORG],  key = "#root.methodName + ':' + #enhetnummer.verdi")
     fun navnFor(enhetnummer: Enhetnummer) = client.enhetFor(enhetnummer.verdi).navn
-
-
 }

73449bbe86720c734b3dc904d51b62b7725c8d1d
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-16 10:47:27 +0200

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,7 +99,6 @@ configurations.configureEach {
 
 dependencies {
     // Kotlin
-    implementation("no.nav.felles:sikkerhetstjenesten-felles:0.0.5")
     implementation(libs.kotlinxCoroutinesCore)
     implementation(libs.kotlinReflect)
     implementation(libs.jackson.module.kotlin)

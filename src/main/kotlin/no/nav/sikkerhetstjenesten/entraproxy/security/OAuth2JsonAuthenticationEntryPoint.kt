41151f9b47c91da6e102619096ab9ffa91d123cb
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-16 09:08:07 +0200

    log

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/security/OAuth2JsonAuthenticationEntryPoint.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/security/OAuth2JsonAuthenticationEntryPoint.kt
@@ -3,6 +3,7 @@ package no.nav.sikkerhetstjenesten.entraproxy.security
 import io.opentelemetry.api.trace.Span
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.http.MediaType
@@ -16,12 +17,16 @@ import java.net.URI
 
 @Component
 class OAuth2JsonAuthenticationEntryPoint(private val mapper: JsonMapper) : AuthenticationEntryPoint {
-    override fun commence(req: HttpServletRequest, res: HttpServletResponse, e: AuthenticationException) =
+    private val log = getLogger(javaClass)
+
+    override fun commence(req: HttpServletRequest, res: HttpServletResponse, e: AuthenticationException) {
+        log.warn("Autentisering feilet for uri={}, message={}", req.requestURI, e.message, e)
         with(res) {
             status = UNAUTHORIZED.value()
             contentType = APPLICATION_PROBLEM_JSON_VALUE
             mapper.writeValue(writer, securityProblemDetail(UNAUTHORIZED, MANGLER_BEARER_TOKEN))
         }
+    }
 
     private fun securityProblemDetail(status: HttpStatus, detail: String) =
         forStatusAndDetail(status, detail).apply {

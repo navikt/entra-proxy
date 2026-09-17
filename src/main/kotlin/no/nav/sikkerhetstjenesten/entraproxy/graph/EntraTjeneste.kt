d2906c4fa4522ebd2b5f1663e42d27a79b866e69
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-17 15:26:43 +0200

    log

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/EntraTjeneste.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/graph/EntraTjeneste.kt
@@ -127,7 +127,7 @@ class EntraTjeneste(private val client: EntraGraphClient, private val norg: Norg
     private fun <T> allSider(førsteSide: T, next: (T) -> URI?, hentSide: (URI) -> T): Sequence<T> =
         generateSequence(førsteSide) { side ->
             next(side)?.let { nesteSideUri ->
-                log.debug("Følger @odata.nextLink {}", nesteSideUri)
+                log.info("Følger @odata.nextLink {}", nesteSideUri)
                 hentSide(nesteSideUri)
             }
         }

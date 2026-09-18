8e8dbbbcde0a6d021caa201c08ef3999b20be8c3
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-18 20:10:17 +0200

    som leder

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/security/OAuth2AuthorityAndRoleAddingJwtAuthenticationConverter.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/security/OAuth2AuthorityAndRoleAddingJwtAuthenticationConverter.kt
@@ -39,9 +39,7 @@ class OAuth2AuthorityAndRoleAddingJwtAuthenticationConverter : Converter<Jwt, Ab
     private fun principal(jwt: Jwt, authorities: Set<GrantedAuthority>) =
         DefaultOAuth2AuthenticatedPrincipal(
             jwt.subject ?: jwt.getClaimAsString(AuthContext.NAVIDENT) ?: "unknown",
-            jwt.claims, authorities)/*.also {
-            log.trace("Principal satt til {} med authorities: {}", it.name, it.authorities)
-        }*/
+            jwt.claims, authorities)
 
     private fun authority(jwt: Jwt) =
         when {

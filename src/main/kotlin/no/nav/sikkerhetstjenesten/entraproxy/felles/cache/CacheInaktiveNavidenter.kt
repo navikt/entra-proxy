e4fc43fcabade49a19b57c0b92a692d1e757466d
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-18 10:01:56 +0200

    oppdater cache etter hver page

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheInaktiveNavidenter.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheInaktiveNavidenter.kt
@@ -25,9 +25,12 @@ class CacheInaktiveNavidenter(private val entra: EntraTjeneste, private val cach
                 cache.replaceSet(INAKTIVE, emptySet())
                 var antall = 0
                 entra.gruppeMedlemmer("$uuid") { side ->
-                    val navIdenter = side.value.mapNotNullTo(mutableSetOf()) { it.onPremisesSamAccountName }
-                    cache.addToSet(INAKTIVE, navIdenter)
-                    antall += navIdenter.size
+                    val sideVarighet = measureTimeMillis {
+                        val navIdenter = side.value.mapNotNullTo(mutableSetOf()) { it.onPremisesSamAccountName }
+                        cache.addToSet(INAKTIVE, navIdenter)
+                        antall += navIdenter.size
+                    }
+                    log.info("Oppdaterte cache med side på {}ms", sideVarighet)
                 }
                 log.info("Periodisk cache-jobb OK, la til {} inaktive medlemmer i cache", cache.getSet(INAKTIVE).size)
             }.onFailure {

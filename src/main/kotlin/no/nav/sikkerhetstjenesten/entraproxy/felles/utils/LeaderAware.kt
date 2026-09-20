721bbd3d03719a91a2f4cf11dc60a1ef8eb504df
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-20 09:36:22 +0200

    slack

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/utils/LeaderAware.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/utils/LeaderAware.kt
@@ -8,18 +8,15 @@ import java.net.InetAddress.getLocalHost
 
 abstract class LeaderAware(private var erLeder: Boolean = false) {
     private val hostname = getLocalHost().hostName
-    protected open fun doHandleLeaderChange() = Unit
+    protected open fun doHandleLeaderChange(nyLeder: String) = Unit
 
     private val log = getLogger(javaClass)
 
     @EventListener(NyLederHendelse::class)
     open fun onApplicationEvent(event: NyLederHendelse) {
         erLeder = event.leder == hostname
-         log.info("Denne instansen er $hostname, lederen er ${event.leder}")
-        somLeder("håndtering av lederbytte", {
-            log.info("Denne instansen ($hostname) er nå leder")
-            doHandleLeaderChange()
-        }) { log.info("Denne instansen ($hostname) er ikke leder, lederen er ${event.leder}") }
+        log.info("Denne instansen er $hostname, lederen er ${event.leder}")
+        doHandleLeaderChange(event.leder)
     }
 
     protected fun somLeder(beskrivelse: String? = null, block: () -> Unit) =

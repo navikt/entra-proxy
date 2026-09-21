7d7b259a4eef28e35c45e6a48581cbec4bd3c8a2
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-21 19:04:45 +0200

    chore

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/leder/LederUtvelger.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/leder/LederUtvelger.kt
@@ -1,6 +1,5 @@
 package no.nav.sikkerhetstjenesten.entraproxy.felles.leder
 
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
@@ -11,11 +10,9 @@ class LederUtvelger(private val sseUtvelger: SSELederUtvelger,
                     private val pollendeUtvelger: PollendeLederUtvelger,
                     private val varsler: LederVarsler) {
 
-    private val log = getLogger(javaClass)
 
     @EventListener(ApplicationReadyEvent::class)
     fun klar() {
-        log.info("Applikasjonen klar")
         sseUtvelger.subscribe { varsler.varsle(it.name) }
         varsler.varsle(pollendeUtvelger.poll(ofSeconds(5))?.name)
     }

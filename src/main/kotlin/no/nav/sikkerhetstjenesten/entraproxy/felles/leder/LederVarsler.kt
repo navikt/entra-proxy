c63fd0c262d7644dfc14c206867fee167c819bac
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-21 19:20:09 +0200

    chore

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/leder/LederVarsler.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/leder/LederVarsler.kt
@@ -10,7 +10,7 @@ class LederVarsler(private val publisher: ApplicationEventPublisher) {
     private val gjeldendeLeder = AtomicReference<String?>(null)
 
     fun varsle(leder: String?) {
-        val ny = leder ?: error("Kunne ikke hente gjeldende leder")
+        val ny = leder ?: error("Kunne ikke varsle om gjeldende leder")
         val gammel = gjeldendeLeder.getAndSet(ny)
         if (gammel != ny) {
             publisher.publishEvent(LederHendelse(this, ny))

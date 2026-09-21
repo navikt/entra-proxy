525c4fdcbe63eab2eab5294d45fd347895151d9e
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-21 18:08:09 +0200

    chore

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/utils/LederUtvelger.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/utils/LederUtvelger.kt
@@ -23,21 +23,21 @@ class LederUtvelger(private val client: WebClient,
     private lateinit var abonnent: Disposable
     private val gjeldendeLeder = AtomicReference<String?>(null)
 
-    private fun abonnerPå(uri: URI) =
+    private fun request(uri: URI) =
         client
             .get()
             .uri(uri)
             .retrieve()
+
+    private fun abonnerPå(uri: URI) =
+        request(uri)
             .bodyToFlux<LederUtvelgerRespons>()
             .map(LederUtvelgerRespons::name)
             .subscribe(::varsleOm) { log.warn("SSE feilet", it) }
 
     private fun gjeldendeLederFra(uri: URI) =
         runCatching {
-            client
-                .get()
-                .uri(uri)
-                .retrieve()
+            request(uri)
                 .bodyToMono<LederUtvelgerRespons>()
                 .block(ofSeconds(5))
                 ?.name
@@ -46,13 +46,12 @@ class LederUtvelger(private val client: WebClient,
         }.getOrThrow()
 
     private fun varsleOm(leder: String?) {
-        leder?.let { ny ->
-            val gammel = gjeldendeLeder.getAndSet(ny)
-            if (gammel != ny) {
-                log.info("Ny leder $ny, gammel var $gammel")
-                publisher.publishEvent(NyLederHendelse(this, ny ))
-            }
-        }?: error("Kunne ikke hente gjeldende leder fra ${cfg.get.url}")
+        val ny = leder ?: error("Kunne ikke hente gjeldende leder fra ${cfg.get.url}")
+        val gammel = gjeldendeLeder.getAndSet(ny)
+        if (gammel != ny) {
+            log.info("Ny leder $ny, gammel var $gammel")
+            publisher.publishEvent(NyLederHendelse(this, ny))
+        }
     }
 
     @EventListener(ApplicationReadyEvent::class)

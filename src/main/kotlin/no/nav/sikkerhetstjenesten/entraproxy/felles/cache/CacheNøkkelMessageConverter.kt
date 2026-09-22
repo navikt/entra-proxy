3ec2dcbd6437048ecfb9aea5fec94ccb67d8d2ec
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-22 21:16:40 +0200

    templateit (#164)

--- src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheNøkkelMessageConverter.kt
+++ src/main/kotlin/no/nav/sikkerhetstjenesten/entraproxy/felles/cache/CacheNøkkelMessageConverter.kt
@@ -3,6 +3,7 @@ package no.nav.sikkerhetstjenesten.entraproxy.felles.cache
 import org.springframework.http.MediaType
 import org.springframework.messaging.Message
 import org.springframework.messaging.converter.AbstractMessageConverter
+import kotlin.text.Charsets.UTF_8
 
 class CacheNøkkelMessageConverter : AbstractMessageConverter(MediaType.APPLICATION_OCTET_STREAM) {
     override fun supports(clazz: Class<*>) =
@@ -11,6 +12,6 @@ class CacheNøkkelMessageConverter : AbstractMessageConverter(MediaType.APPLICAT
     override fun convertFromInternal(message: Message<*>, targetClass: Class<*>, conversionHint: Any?) =
         (message.payload as? ByteArray)
             ?.takeIf { targetClass == CacheNøkkel::class.java }
-            ?.toString(Charsets.UTF_8)
+            ?.toString(UTF_8)
             ?.let(::CacheNøkkel)
 }
